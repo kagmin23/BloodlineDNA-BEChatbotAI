d9ff5841d0fb9002ad0091d9102461deeeff28bd
KagMin <phankangmin@gmail.com>
2025-07-12 01:58:18 +0700

    Update chat.routes.js

--- routes/chat.routes.js
+++ routes/chat.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { sendMessage, getChatHistory } = require("../controllers/chat.controller");
-
+// Chat
 router.post("/chat", sendMessage);
 router.get("/history", getChatHistory);
 
